00cbf9fa7e8cab70a8a8d6d1730635f6ffee6358
jhangmez <jhangomez25@gmail.com>
2023-12-11 23:42:12 -0500

    Se modifico par que redirija a la pagina de tuti

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,47 +1,11 @@
 'use client'
 
-import Form from '@components/Form'
-import Link from 'next/link'
-import { Icon } from '@iconify/react'
+import { redirect } from 'next/navigation'
 
 export default function Login() {
-  return (
-    <main className='flex flex-col md:flex-row-reverse md:h-screen bg-light-surface dark:bg-dark-surface h-screen'>
-      <section className='flex items-start w-full px-4 mx-auto md:px-0 md:items-center md:w-1/3'>
-        <div className=' relative md:-left-2 bg-light-surface dark:bg-dark-surface pt-5 py-5'>
-          <Link
-            href='/'
-            className='w-fit h-14 justify-start items-center gap-[5px] inline-flex'
-          >
-            <Icon
-              icon='material-symbols:store'
-              width='28'
-              height='28'
-              className='text-light-onSurface dark:text-dark-onSurface'
-            />
-            <div>
-              <span className='text-light-onSurface dark:text-dark-onSurface text-2xl font-bold leading-[44px]'>
-                ronal
-              </span>
-              <span className='text-light-primary dark:text-dark-primary text-2xl font-bold leading-[44px]'>
-                Tienda
-              </span>
-            </div>
-          </Link>
-        </div>
-      </section>
-      <section className='justify-center px-4 md:px-0 md:flex md:w-2/3 md:border-r'>
-        <div className='w-full max-w-sm py-4 mx-auto my-auto min-w-min md:py-9 md:w-7/12'>
-          <h2 className='text-xl font-semibold md:text-2xl text-light-onSurface dark:text-dark-onSurface'>
-            Ingresar
-          </h2>
-          <p className='text-sm text-light-onSurface dark:text-dark-onSurface pb-2'>
-            Usa tu correo electrónico y contraseña para ingresar.
-          </p>
-
-          <Form type='login' />
-        </div>
-      </section>
-    </main>
-  )
+  if (process.env.TUTI_HARKAYSOFT_LINK) {
+    return redirect(process.env.TUTI_HARKAYSOFT_LINK + '/login')
+  } else {
+    redirect('/')
+  }
 }
